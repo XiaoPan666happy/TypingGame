01b09e2f06f1ab0c6a3c5743acb3c1fbcde5ff46
XiaoPan666 <matthew2023@126.com>
2024-11-16 13:29:20 +0800

    完成封面制作

--- main.py
+++ main.py
@@ -4,21 +4,31 @@ import pygame
 from config import *
 
 pygame.init()
+pygame.font.init()
 
-def start(screen:pygame.surface.Surface) -> bool:
+def start(screen:pygame.surface.Surface, 
+          font_big:pygame.font.Font,
+          font_small:pygame.font.Font) -> bool:
     """封面"""
     while not(pygame.key.get_pressed()[pygame.K_RETURN]):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
-    
+
         # 没啥逻辑 把背景渲染一下
-        screen.fill((200, 200, 255))
+        text1 = font_big.render("打字游戏", True, (63, 72, 204))
+        text2 = font_small.render("Please pressed enter and play the game", True, (63, 72, 204))
+        screen.fill((255, 255, 255))
+        screen.blit(text1, (200, 200))
+        screen.blit(text2, (200, 350))
+
         pygame.display.flip()
-        
+
     return True
 
-def play_game(screen:pygame.surface.Surface) -> None:
+def play_game(screen:pygame.surface.Surface, 
+              font:pygame.font.Font,
+              font_small:pygame.font.Font) -> None:
     """主函数"""
     pass
 
@@ -26,10 +36,13 @@ def main() -> None:
     """控制函数"""
     screen = pygame.display.set_mode(SCREEN_SIZE)
     pygame.display.set_caption("打字游戏")
-    if start(screen):
-        play_game(screen)
+    font_big = pygame.font.Font(os.path.join(PATH, "font", "STKAITI.TTF"), 100)
+    font_small = pygame.font.Font(os.path.join(PATH, "font", "STKAITI.TTF"), 25)
+    if start(screen, font_big, font_small):
+        play_game(screen, font_big, font_small)
 
 if __name__ == "__main__":
     main()
 
+pygame.font.quit()
 pygame.quit()
