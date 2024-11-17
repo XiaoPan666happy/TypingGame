31c5e7659c9db1d6cd1c29414ce2db7cbaf8f9d7
XiaoPan666 <matthew2023@126.com>
2024-11-17 21:13:31 +0800

    完成主函数中的状态显示

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 SCREEN_SIZE = (800, 600)
+FPS = 100
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 """主程序文件"""
 
+import time
+
 import pygame
+
 from config import *
 
 pygame.init()
@@ -19,31 +22,49 @@ def start(screen:pygame.surface.Surface,
         text1 = font_big.render("打字游戏", True, (63, 72, 204))
         text2 = font_small.render("Please pressed enter and play the game", True, (63, 72, 204))
         screen.fill((255, 255, 255))
-        screen.blit(text1, (200, 175))
-        screen.blit(text2, (220, 325))
+        screen.blit(text1, (195, 175))
+        screen.blit(text2, (215, 325))
         pygame.display.flip()
 
     return True
 
 def play_game(screen:pygame.surface.Surface, 
-              font:pygame.font.Font,
+              font_big:pygame.font.Font,
               font_small:pygame.font.Font) -> None:
     """主函数"""
     
-    score = 0   # 得分
-    miss = 0    # 遗漏
-    combo = 0   # 连击
-    time = 0.0  # 时间
-
+    score = 0        # 得分
+    miss = 0         # 遗漏
+    combo = 0        # 连击
+    play_time = 0.0  # 时间
+    
+    start_time = time.time()
+    clock = pygame.time.Clock()
     running = True
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         
+        play_time = round((time.time() - start_time), 1)
+        fps = int(clock.get_fps())
+
+        text_fps = font_small.render(f"帧率    {fps}", True, (0, 0, 0))
+        text_score = font_small.render(f"得分    {score}", True, (0, 0, 0))
+        text_miss = font_small.render(f"遗漏    {miss}", True, (0, 0, 0))
+        text_time = font_small.render(f"时间    {play_time}", True, (0, 0, 0))
+
         # 渲染
         screen.fill((255, 255, 255))
+        screen.blit(text_fps, (10, 10))
+        screen.blit(text_score, (10, 40))
+        screen.blit(text_miss, (10, 70))
+        screen.blit(text_time, (10, 100))
         pygame.display.flip()
+
+        # 控制帧率
+        clock.tick(FPS)
 
 def init() -> tuple:
     """初始化"""
